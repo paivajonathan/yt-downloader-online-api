4b516b66728f519f724f0f3159b4972937996704
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-04-05 00:37:19 -0300

    test: add test

--- core/api.py
+++ core/api.py
@@ -2,5 +2,10 @@ from ninja import NinjaAPI
 
 
 api = NinjaAPI()
+
+@api.get('teste/')
+def teste():
+    return 'teste'
+
 api.add_router('video/', 'video.api.router')
 api.add_router('audio/', 'audio.api.router')
