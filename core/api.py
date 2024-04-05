f5b4e4692bd053bc26ef695c30e11f3bfc3b3114
Jonathan Paiva <jonathanapaiva@gmail.com>
2024-04-05 00:51:32 -0300

    test: remove test

--- core/api.py
+++ core/api.py
@@ -3,9 +3,5 @@ from ninja import NinjaAPI
 
 api = NinjaAPI()
 
-@api.get('teste/')
-def teste():
-    return 'teste'
-
 api.add_router('video/', 'video.api.router')
 api.add_router('audio/', 'audio.api.router')
